f99c1996ee39e751f772d6c3718828b23ccd5b8c
raohamza10 <hamzarao2004@gmail.com>
2025-05-04 17:01:46 +0500

    Initial commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div>
+      <div className='text-slate-600 font-bold text-lg max-w-md m-auto flex  flex-col justify-center items-center h-28 bg-slate-200 rounded'>
+        <p>About</p>
+        <p className='text-base text-green-600'>Added soon...</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+  return (
+    <div>
+      <div className='text-slate-600 font-bold text-lg max-w-md m-auto flex  flex-col justify-center items-center h-28 bg-slate-200 rounded'>
+        <p>Contact</p>
+        <p className='text-base text-green-600'>Added soon...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
